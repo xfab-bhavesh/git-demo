aa149fa6888f4e8145cf5abcc6728cfcd1a92505
Bhavesh Sidhwani <bhavesh@xfabric.ai>
2025-05-22 12:16:54 -0700

    Minor change

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   tags = {
-    Name = "Terraform-VM"
+    Name = "Terraform-VM-1"
   }
 }
 # RDS Database (PostgreSQL)
