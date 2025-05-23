89f9bfdd3304fbdb819c7c663c3d38baed02a36f
xfab-bhavesh <bhavesh@xfabric.ai>
2025-05-22 23:58:54 -0700

    Updated EC2 instance name

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
   region = "us-east-1"
 }
 # EC2 Instance
-resource "aws_instance" "Test Instance 3" {
+resource "aws_instance" "test_instance1" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   tags = {
